2135ab2031705e3a81b577189d4c5a984839b4dd
keidsondesigner <keidsondesigner@gmail.com>
2022-09-30 19:52:48 -0400

    inital commit

---
+++ android/app/src/main/java/com/keidson/testNovo/MainActivity.java
@@ -0,0 +1,5 @@
+package com.keidson.testNovo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'testeNovo',
+  appId: 'com.keidson.testNovo',
+  appName: 'Teste Novo',
   webDir: 'www',
   bundledWebRuntime: false
 };
